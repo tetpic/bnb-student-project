ed09712a57554a954e282c17696d3695b693125c
tetpic <bull1604@yandex.ru>
2021-12-13 18:37:24 +0300

    adaptive pixel perfect done

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-let leftButton = document.querySelectorAll('.left-button');
-let rightButton = document.querySelectorAll('.right-button');
+let leftButton = document.querySelectorAll('.left-button')
+let rightButton = document.querySelectorAll('.right-button')
 
 
 
@@ -20,7 +20,7 @@ let swiper = new Swiper('.company-subdivisions', {
  
 
   
-  let navBlocks = document.querySelectorAll('.navigation__block');
+  let navBlocks = document.querySelectorAll('.navigation__block')
   let backgroundImages = document.querySelectorAll('.main-background__image')
   
 swiper.on('activeIndexChange', function () {
@@ -35,12 +35,12 @@ swiper.on('activeIndexChange', function () {
 
 navBlocks.forEach(function(navBlock){
   navBlock.addEventListener('click', function(event){
-    i = Array.from(navBlocks).indexOf(event.target );
+    i = Array.from(navBlocks).indexOf(event.target )
     swiper.slideTo(i);
     });
-  })
+})
 
-  let burgerMenuOn = document.querySelector('.burger')
+let burgerMenuOn = document.querySelector('.burger')
 let burgerMenuOff = document.querySelector('.burger-off')
 let modalWindow = document.querySelector('.modal-window')
 
@@ -55,67 +55,63 @@ burgerMenuOff.addEventListener('click', function() {
 } )
 
 
-  let swiper2 = new Swiper('.our-events-sections', {
-    spaceBetween: 5,
-    navigation: {
-      nextEl: '.next',
-      prevEl: '.prev',
-    },
-    
-    wrapperClass: 'swiper-wrapper',
-    slideClass: 'event-section',
-    autoHeight: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      640: {
-        slidesPerView: 3,
-      },
-    }, 
-    slidesPerView: 3,
-    setWrapperSize: true,
-  });
+let swiper2 = new Swiper('.our-events-sections', {
+  // spaceBetween: 5,
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },  
 
+  spaceBetween: 10,
+  slidesPerView: 'auto',
+  freeMode: {
+    enabled: true,
+  },
 
- 
-
-
-
+  wrapperClass: 'swiper-wrapper',
+  slideClass: 'event-section',
+  // autoHeight: true,
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //   },
+  //   480: {
+  //     slidesPerView: 2,
+  //   },
+  //   640: {
+  //     slidesPerView: 3,
+  //   },
+  // }, 
+  // slidesPerView: 3,
+  // setWrapperSize: true,
+});
 
 let roomTypes = document.querySelectorAll('.room__heading')
 let roomBlocks = document.querySelectorAll('.room-block')
-// let karaoke = document.querySelectorAll('._karaoke')
-// let individual = document.querySelectorAll('._individual')
-// let banket = document.querySelectorAll('._banket')
-// let public = document.querySelectorAll('.public')
-
-
-
-
 
 roomTypes.forEach(function (roomType) {
   roomType.addEventListener('click', function(event) {
 
     activeIndex = Array.from(roomTypes).indexOf(event.target)
 
-    const every = 0;
-    const banketRooms = 1;
-    const publicRooms = 2;
-    const individualRooms = 3;
-    const karaokeRooms = 4;
+    const every = 0
+    const banketRooms = 1
+    const publicRooms = 2
+    const individualRooms = 3
+    const karaokeRooms = 4
 
     roomBlocks.forEach(function(activate){
       activate.classList.remove('disabled')
     })
     roomTypes.forEach(function(disable) {
-      disable.classList.remove('section__heading_active')
+      disable.classList.remove('room__heading_active')
     })
 
-    roomTypes[activeIndex].classList.add('section__heading_active')
+    roomTypes[activeIndex].classList.add('room__heading_active')
 
     if (activeIndex == banketRooms) {
       document.querySelectorAll('._karaoke').forEach(function(disable){
@@ -149,29 +145,40 @@ roomTypes.forEach(function (roomType) {
 
 
 let roomSwiper = new Swiper ('.room-types-slider', {
-  // wrapperClass: 'swiper-wrapper',
-  // slideClass: 'swiper-slide',
   spaceBetween: 30,
-  // setWrapperSize: true,
-  // freeMode: true,
   slidesPerView: "auto",
   scrollbar: {
     el: ".swiper-scrollbar",
-    // hide: true,
   },
   breakpoints: {
     320: {
       allowTouchMove: true,
-      // slidesPerView: 3,
     },
     480: {
       allowTouchMove: true,
-      // slidesPerView: 3,
     },
     880: {
       allowTouchMove: false,
-      // enabled: false,
     },
   },
+})
 
-});
+
+let reportSwiper = new Swiper ('.photo-reports', {
+  spaceBetween: 10,
+  slidesPerView: 'auto',
+  freeMode: {
+    enabled: true,
+  },
+  breakpoints: {
+    320: {
+      allowTouchMove: true,
+    },
+    500: {
+      allowTouchMove: true,
+    },
+    1150: {
+      allowTouchMove: false,
+    },
+  },
+})
