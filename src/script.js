5ccc698f19b7db0f69f32c8563f6f09ec77f296b
tetpic <bull1604@yandex.ru>
2021-12-15 15:14:51 +0300

    softly makin fisting love with js

--- src/script.js
+++ src/script.js
@@ -92,17 +92,23 @@ let swiper2 = new Swiper('.our-events-sections', {
 
 let roomTypes = document.querySelectorAll('.room__heading')
 let roomBlocks = document.querySelectorAll('.room-block')
+let pagination = document.querySelector('.__pagination_string');
+
+
+let setDefaultPagination = () => {
+  let paginationDefault = roomTypes[0].getBoundingClientRect()
+  pagination.style.width = (paginationDefault.width) + 'px'
+};
+
+setDefaultPagination();
+
 
 roomTypes.forEach(function (roomType) {
   roomType.addEventListener('click', function(event) {
 
     activeIndex = Array.from(roomTypes).indexOf(event.target)
 
-    const every = 0
-    const banketRooms = 1
-    const publicRooms = 2
-    const individualRooms = 3
-    const karaokeRooms = 4
+  
 
     roomBlocks.forEach(function(activate){
       activate.classList.remove('disabled')
@@ -111,7 +117,28 @@ roomTypes.forEach(function (roomType) {
       disable.classList.remove('room__heading_active')
     })
 
-    roomTypes[activeIndex].classList.add('room__heading_active')
+    roomTypes[activeIndex].classList.add('room__heading_active');
+
+    console.log(roomTypes[activeIndex]);
+
+    let roomTypeProperties = roomTypes[activeIndex].getBoundingClientRect()
+
+    console.log(roomTypeProperties.left);
+    
+
+    
+    pagination.style.transitionDuration = '0.3s';
+    pagination.style.width = (roomTypeProperties.width) + 'px';
+    // pagination.style.transform = 'translateX('+ (roomTypeProperties.left)+ 'px)'
+    pagination.style.left = (roomTypeProperties.left) + 'px';
+
+    // pagination.style.transform = 'translateX('+(roomTypeProperties.left)+')'
+    
+    const every = 0
+    const banketRooms = 1
+    const publicRooms = 2
+    const individualRooms = 3
+    const karaokeRooms = 4
 
     if (activeIndex == banketRooms) {
       document.querySelectorAll('._karaoke').forEach(function(disable){
@@ -144,24 +171,24 @@ roomTypes.forEach(function (roomType) {
 })})
 
 
-let roomSwiper = new Swiper ('.room-types-slider', {
-  spaceBetween: 30,
-  slidesPerView: "auto",
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  breakpoints: {
-    320: {
-      allowTouchMove: true,
-    },
-    480: {
-      allowTouchMove: true,
-    },
-    880: {
-      allowTouchMove: false,
-    },
-  },
-})
+// let roomSwiper = new Swiper ('.room-types-slider', {
+//   spaceBetween: 30,
+//   slidesPerView: "auto",
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+//   breakpoints: {
+//     320: {
+//       allowTouchMove: true,
+//     },
+//     480: {
+//       allowTouchMove: true,
+//     },
+//     880: {
+//       allowTouchMove: false,
+//     },
+//   },
+// })
 
 
 let reportSwiper = new Swiper ('.photo-reports', {
@@ -181,4 +208,40 @@ let reportSwiper = new Swiper ('.photo-reports', {
       allowTouchMove: false,
     },
   },
+})
+
+
+
+let basketCounter = document.querySelector('.basket__counter')
+
+
+
+let basketCounterLength = () => {
+  // basketCounter.classList.add('disabled')
+  basketCounter.innerText = prompt('сколько будем покупать?')
+  
+  if (+(basketCounter.innerText) == +0) {
+    
+    basketCounter.classList.add('disabled')
+  }
+  else if (+(basketCounter.innerText) >= +1) {
+    
+    basketCounter.classList.remove('disabled')
+  }
+  else {
+    console.log('error')
+  };
+  if (+(basketCounter.innerText) >= 100) {
+   basketCounter.innerText = '99+'
+ }
+};
+basketCounterLength()
+
+let sectionDescription = document.querySelectorAll('.section__description')
+
+sectionDescription.forEach(function (bigContent) {
+  sectionIndex = Array.from(sectionDescription)
+  if (+(sectionDescription[e].innerText.length) >= 100 {
+    
+  }
 })
